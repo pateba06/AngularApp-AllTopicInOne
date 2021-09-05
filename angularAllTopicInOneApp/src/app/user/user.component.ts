79699a68ae14e4555d42f1f6810b885f27ea2f28
pateba06 <34305933+pateba06@users.noreply.github.com>
2021-09-05 13:04:28 -0400

    created first component

--- angularAllTopicInOneApp/src/app/user/user.component.ts
+++ angularAllTopicInOneApp/src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-user',
-    templateUrl:'./user.component.html'
+    templateUrl:'./user.component.html',
+    styleUrls:['./user.component.css']
 })
 export class UserComponent {}
