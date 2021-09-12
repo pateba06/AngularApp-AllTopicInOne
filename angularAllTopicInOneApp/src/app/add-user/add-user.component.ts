463cf31f4ccec302fafa92c7a69f8111d9a3df40
pateba06 <34305933+pateba06@users.noreply.github.com>
2021-09-11 21:53:08 -0400

    12-usingServiceAsData - Example

--- angularAllTopicInOneApp/src/app/add-user/add-user.component.ts
+++ angularAllTopicInOneApp/src/app/add-user/add-user.component.ts
@@ -1,14 +1,16 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css'],
 })
-export class AddUserComponent implements OnInit {
+export class AddUserComponent {
 
-  constructor() {}
-
-  ngOnInit() {}
-
+  userName : string;
+  constructor(private userService:UserService) {}
+  onAddUdser(){
+    this.userService.addUser(this.userName,'active')
+  }
 }
--- angularAllTopicInOneApp/src/app/services/user.service.ts
+++ angularAllTopicInOneApp/src/app/services/user.service.ts
@@ -11,4 +11,14 @@ export class UserService {
     { name: 'Dhaval', status: 'Active' },
     { name: 'Sheetal', status: 'Active' },
   ];
+
+  // method to add user
+  addUser( name:string , status:string){
+    this.users.push({name,status});
+  }
+
+  // method to change userStatus by using id
+  updateStatus(id:number , status:string){
+    this.users[id].status = status;
+  }
 }
