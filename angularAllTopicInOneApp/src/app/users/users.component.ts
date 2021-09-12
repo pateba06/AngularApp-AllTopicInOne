b7be5cc4b8dbc779648c540ea47b48279e41eecf
pateba06 <34305933+pateba06@users.noreply.github.com>
2021-09-12 13:03:29 -0400

    17 - navigating using Router programmatically in typescript code

--- angularAllTopicInOneApp/src/app/users/users.component.ts
+++ angularAllTopicInOneApp/src/app/users/users.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // option1 -  navigate using router -navigateByURL
+  onCategoriesClick1() {
+    // perform some logig and then navigagte to page
+    this.router.navigateByUrl('/categories');
   }
 
+  // option 2 -  navigate using router -navigate
+  onCategoriesClick2() {
+    // it is property so we have to give array type of data. We can also pass id
+    this.router.navigate(['/categories']);
+  }
 }
